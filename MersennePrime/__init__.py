94d15918c67c15190e9b00d0884f88902f88ac1b
David Lednik <david.lednik@gmail.com>
2018-07-01 03:28:33 +0100

    multiply working

--- MersennePrime/__init__.py
+++ MersennePrime/__init__.py
@@ -37,24 +37,32 @@ class MersennePrime:
         for i in range(len(s)):
             m[i] -= s[i]
         for i in range(len(m)-1):
-            if m[i] < 0:
+            while m[i] < 0:
                 m[i] += 256
                 m[i+1] -= 1
         if m[-1] < 0:
             raise Exception('Negative value.')
         while m[-1] == 0:
+            if len(m) == 1:
+                break
             del m[-1]
-        if m == []:
-            return [0]
         return m
 
     def _add(self, n, a):
         m = n[:]
-        m[0] += a
+        while len(m) < len(a):
+            m.append(0)
+        for i in range(len(a)):
+            m[i] += a[i]
         for i in range(len(m)-1):
-            if m[i] < 0:
-                m[i] += 256
-                m[i+1] -= 1
+            while m[i] > 255:
+                m[i] -= 256
+                m[i+1] += 1
+        if m[-1] > 255:
+            t = self._list(m[-1])
+            m[-1] = t[0]
+            for i in range(1, len(t)):
+                m.append(t[i])
         return m
 
     def _and(self, n, a):
@@ -65,6 +73,10 @@ class MersennePrime:
             l1.append(0)
         for i in range(len(l2)):
             m.append(l1[i] & l2[i])
+        while m[-1] == 0:
+            if len(m) == 1:
+                break
+            del m[-1]
         return m
 
     def _smaller(self, n, m):
@@ -89,8 +101,51 @@ class MersennePrime:
             r = self._sub(r, m)
         return r
 
+    def _mul(self, n, m):
+        r = n[:]
+        w = n[:]
+        q = m[:]
+        while len(r)<len(m):
+            r.append(0)
+        while len(q)<len(r):
+            q.append(0)
+        for i in range(len(r)):
+            r[i] = 0
+        while len(w)<len(r):
+            w.append(0)
+        for i in range(len(w)):
+            for j in range(len(q)):
+                r[i+j] += w[i] * q[j]
+            r.append(0)
+
+        while r[-1] == 0:
+            if len(r) == 1:
+                break
+            del r[-1]
+        
+        for i in range(len(r)-1):
+            while r[i] > 255:
+                r[i] -= 256
+                r[i+1] += 1
+        if r[-1] > 255:
+            t = self._list(r[-1])
+            r[-1] = t[0]
+            for i in range(1, len(t)):
+                r.append(t[i])
+        
+        return r
+
+    def _square(self, n):
+        return self._mul(n[:], n[:])
+
     def __init__(self, m):
-        self._m = 11
+        if m<2:
+            raise Exception('Number should be bigger than 2.')
+        if m%2 != 1:
+            raise Exception('Number should be odd.')
+        # if m is not prime:
+        # raise Esxception('No time wasters please. I can only accept prime numbers.')
+        self._m = m
         # P = 2 ** m - 1
         self._P = self._list(m)
     
